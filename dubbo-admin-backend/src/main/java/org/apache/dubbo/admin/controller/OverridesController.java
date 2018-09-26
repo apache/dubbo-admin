02097cae4c4d0b0035a567d08e0f80812b1ae21d
nzomkxia <z82507200@gmail.com>
2018-09-26 16:56:17 +0800

    update copyright

--- dubbo-admin-backend/src/main/java/org/apache/dubbo/admin/controller/OverridesController.java
+++ dubbo-admin-backend/src/main/java/org/apache/dubbo/admin/controller/OverridesController.java
@@ -15,25 +15,8 @@
  * limitations under the License.
  */
 
-package org.apache.dubbo.admin.util;
+package org.apache.dubbo.admin.controller;
 
-import org.yaml.snakeyaml.Yaml;
-
-import java.util.Map;
-
-public class YamlUtil {
-
-    private static Yaml yaml;
-
-    static {
-        yaml = new Yaml();
-    }
-
-    public static Map<String, Object> loadString(String text) {
-        if (text != null) {
-            return yaml.load(text);
-        }
-        return null;
-    }
+public class OverridesController {
 
 }
