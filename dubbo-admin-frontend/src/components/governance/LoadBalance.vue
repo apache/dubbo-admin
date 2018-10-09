fe7dea7038b78c5f7605072108c4ca25f9a8d707
nzomkxia <z82507200@gmail.com>
2018-10-09 22:10:18 +0800

    use search component

--- dubbo-admin-frontend/src/components/governance/LoadBalance.vue
+++ dubbo-admin-frontend/src/components/governance/LoadBalance.vue
@@ -19,15 +19,8 @@
   <v-container grid-list-xl fluid >
     <v-layout row wrap>
       <v-flex xs12 >
-        <v-card flat color="transparent">
-          <v-card-text>
-            <v-layout row wrap >
-              <v-text-field label="Search dubbo service"
-                            v-model="filter" clearable></v-text-field>
-              <v-btn @click="submit" color="primary" large>Search</v-btn>
-            </v-layout>
-          </v-card-text>
-        </v-card>
+        <search v-model="filter" :submit="submit" label="Search dubbo service"></search>
+
       </v-flex>
     </v-layout>
 
@@ -111,9 +104,11 @@
   import yaml from 'js-yaml'
   import {AXIOS} from '../http-common'
   import AceEditor from '@/components/public/AceEditor'
+  import Search from '@/components/public/Search'
   export default {
     components: {
-      AceEditor
+      AceEditor,
+      Search
     },
     data: () => ({
       dropdown_font: [ 'Service', 'App', 'IP' ],
