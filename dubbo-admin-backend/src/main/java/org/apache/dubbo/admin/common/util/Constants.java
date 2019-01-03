8e11a0d00671de70f4c6f5322dcf7ceeb82bbf0b
zhanghuilong <zhanghuilong@corp.netease.com>
2019-01-03 18:33:57 +0800

    1.add equals(),hashCode() to ServiceDTO 2.use admin Constants instead of dubbo-parent Constants

--- dubbo-admin-backend/src/main/java/org/apache/dubbo/admin/common/util/Constants.java
+++ dubbo-admin-backend/src/main/java/org/apache/dubbo/admin/common/util/Constants.java
@@ -41,6 +41,15 @@ public class Constants {
     public static final String BALANCING = "balancing";
     public static final String SERVICE = "service";
     public static final String APPLICATION = "application";
+
+    public static final String PUNCTUATION_POINT = ".";
+    public static final String PUNCTUATION_SEPARATOR_POINT = "\\.";
+    public final static String INTERROGATION_POINT = "?";
+    public static final String ANY_VALUE = "*";
+    public static final String IP = "ip";
+    public static final String INTERFACE_KEY = "interface";
+    public static final String VERSION_KEY = "version";
+
     public static final Set<String> CONFIGS = new HashSet<>();
 
     static {
