15386f9aabfdbb1f4c65af1376701a6407c6d89b
Xin Wang <xin.victorwang@gmail.com>
2019-01-20 20:52:21 +0800

    Update Search.spec.js

--- dubbo-admin-frontend/test/components/public/Search.spec.js
+++ dubbo-admin-frontend/test/components/public/Search.spec.js
@@ -3,7 +3,7 @@ import Search from '@/components/public/Search'
 describe('Search.vue', () => {
 
   it('displays items from the list', () => {
-    expect(search.data.leng).toBe(0)
+    expect(search.data.leng).toBe(100)
   })
 
 })
