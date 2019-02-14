c6aeb8f5b0b4c5dd3e1627296cdae09df9a264a5
kezhenxu94 <kezhenxu94@163.com>
2019-02-14 18:19:50 +0800

    create adapter for util method

--- dubbo-admin-server/src/main/java/org/apache/dubbo/admin/common/util/OverrideUtils.java
+++ dubbo-admin-server/src/main/java/org/apache/dubbo/admin/common/util/OverrideUtils.java
@@ -37,32 +37,6 @@ import java.util.Map;
  *
  */
 public class OverrideUtils {
-    public static List<Weight> overridesToWeights(List<Override> overrides) {
-        List<Weight> weights = new ArrayList<Weight>();
-        if (overrides == null) {
-            return weights;
-        }
-        for (Override o : overrides) {
-            if (StringUtils.isEmpty(o.getParams())) {
-                continue;
-            } else {
-                Map<String, String> params = StringUtils.parseQueryString(o.getParams());
-                for (Map.Entry<String, String> entry : params.entrySet()) {
-                    if (entry.getKey().equals("weight")) {
-                        Weight weight = new Weight();
-                        weight.setAddress(o.getAddress());
-                        weight.setId(o.getId());
-                        weight.setHash(o.getHash());
-                        weight.setService(o.getService());
-                        weight.setWeight(Integer.valueOf(entry.getValue()));
-                        weights.add(weight);
-                    }
-                }
-            }
-        }
-        return weights;
-    }
-
     public static OverrideConfig weightDTOtoConfig(WeightDTO weightDTO) {
         OverrideConfig overrideConfig = new OverrideConfig();
         overrideConfig.setType(Constants.WEIGHT);
@@ -95,34 +69,6 @@ public class OverrideUtils {
         }
         dynamicConfigDTO.setEnabled(overrideDTO.isEnabled());
         return dynamicConfigDTO;
-    }
-    public static OverrideDTO createFromDynamicConfig(DynamicConfigDTO dynamicConfigDTO) {
-        OverrideDTO overrideDTO = new OverrideDTO();
-        if (StringUtils.isNotEmpty(dynamicConfigDTO.getApplication())) {
-            overrideDTO.setScope(Constants.APPLICATION);
-            overrideDTO.setKey(dynamicConfigDTO.getApplication());
-        } else {
-            overrideDTO.setScope(Constants.SERVICE);
-            overrideDTO.setKey(dynamicConfigDTO.getService());
-        }
-        overrideDTO.setConfigVersion(dynamicConfigDTO.getConfigVersion());
-        overrideDTO.setConfigs(dynamicConfigDTO.getConfigs());
-        return overrideDTO;
-    }
-
-    public static OverrideConfig balanceDTOtoConfig(BalancingDTO balancingDTO) {
-        OverrideConfig overrideConfig = new OverrideConfig();
-        overrideConfig.setType(Constants.BALANCING);
-        overrideConfig.setEnabled(true);
-        overrideConfig.setSide(Constants.CONSUMER_SIDE);
-        Map<String, Object> parameters = new HashMap<>();
-        if (balancingDTO.getMethodName().equals("*")) {
-            parameters.put("loadbalance", balancingDTO.getStrategy());
-        } else {
-            parameters.put(balancingDTO.getMethodName() + ".loadbalance", balancingDTO.getStrategy());
-        }
-        overrideConfig.setParameters(parameters);
-        return overrideConfig;
     }
 
     public static WeightDTO configtoWeightDTO(OverrideConfig config, String scope, String key) {
@@ -157,82 +103,4 @@ public class OverrideUtils {
         }
         return balancingDTO;
     }
-
-    public static Weight overrideToWeight(Override override) {
-        List<Weight> weights = OverrideUtils.overridesToWeights(Arrays.asList(override));
-        if (weights != null && weights.size() > 0) {
-            return overridesToWeights(Arrays.asList(override)).get(0);
-        }
-        return null;
-    }
-
-    public static Override weightToOverride(Weight weight) {
-        Override override = new Override();
-        override.setId(weight.getId());
-        override.setHash(weight.getHash());
-        override.setAddress(weight.getAddress());
-        override.setEnabled(true);
-        override.setParams("weight=" + weight.getWeight());
-        override.setService(weight.getService());
-        return override;
-    }
-
-    public static List<LoadBalance> overridesToLoadBalances(List<Override> overrides) {
-        List<LoadBalance> loadBalances = new ArrayList<>();
-        if (overrides == null) {
-            return loadBalances;
-        }
-        for (Override o : overrides) {
-            if (StringUtils.isEmpty(o.getParams())) {
-                continue;
-            } else {
-                Map<String, String> params = StringUtils.parseQueryString(o.getParams());
-                for (Map.Entry<String, String> entry : params.entrySet()) {
-                    if (entry.getKey().endsWith("loadbalance")) {
-                        LoadBalance loadBalance = new LoadBalance();
-                        String method = null;
-                        if (entry.getKey().endsWith(".loadbalance")) {
-                            method = entry.getKey().split(".loadbalance")[0];
-                        } else {
-                            method = "*";
-                        }
-
-                        loadBalance.setMethod(method);
-                        loadBalance.setId(o.getId());
-                        loadBalance.setHash(o.getHash());
-                        loadBalance.setService(o.getService());
-                        loadBalance.setStrategy(entry.getValue());
-                        loadBalances.add(loadBalance);
-
-                    }
-                }
-            }
-        }
-        return loadBalances;
-    }
-
-    public static LoadBalance overrideToLoadBalance(Override override) {
-        List<LoadBalance> loadBalances = OverrideUtils.overridesToLoadBalances(Arrays.asList(override));
-        if (loadBalances != null && loadBalances.size() > 0) {
-            return loadBalances.get(0);
-        }
-        return null;
-    }
-
-    public static Override loadBalanceToOverride(LoadBalance loadBalance) {
-        Override override = new Override();
-        override.setId(loadBalance.getId());
-        override.setHash(loadBalance.getHash());
-        override.setService(loadBalance.getService());
-        override.setEnabled(true);
-        String method = loadBalance.getMethod();
-        String strategy = loadBalance.getStrategy();
-        if (StringUtils.isEmpty(method) || method.equals("*")) {
-            override.setParams("loadbalance=" + strategy);
-        } else {
-            override.setParams(method + ".loadbalance=" + strategy);
-        }
-        return override;
-    }
-
 }
