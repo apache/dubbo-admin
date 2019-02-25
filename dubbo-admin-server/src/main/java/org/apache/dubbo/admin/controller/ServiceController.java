ef9e050dca2003be2dfe217fa3d46f1f5cbd5dcc
kezhenxu94 <kezhenxu94@163.com>
2019-02-25 23:23:19 +0800

    remove unused import

--- dubbo-admin-server/src/main/java/org/apache/dubbo/admin/controller/ServiceController.java
+++ dubbo-admin-server/src/main/java/org/apache/dubbo/admin/controller/ServiceController.java
@@ -38,7 +38,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
