f648757ae97c4324eceab7b40ad61e916d36651b
nzomkxia <z82507200@gmail.com>
2019-03-18 11:26:41 +0800

    update copyright

--- dubbo-admin-distribution/src/bin-distribution-check.sh
+++ dubbo-admin-distribution/src/bin-distribution-check.sh
@@ -1,9 +1,24 @@
-#!bin/sh
+#Licensed to the Apache Software Foundation (ASF) under one or more
+#contributor license agreements.  See the NOTICE file distributed with
+#this work for additional information regarding copyright ownership.
+#The ASF licenses this file to You under the Apache License, Version 2.0
+#(the "License"); you may not use this file except in compliance with
+#the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
+#! /bin/sh
 
 # HOW TO RUN THIS SCRIPT
 # 1. unzip the bin-release file.
 # 2. place this script under your unzipped bin-release directory.
-# 3. if you want to check the javascript dependencies, 
+# 3. if you want to check the javascript dependencies,
 #    copy the package.json file to the directory.
 # 4. run this script.
 
