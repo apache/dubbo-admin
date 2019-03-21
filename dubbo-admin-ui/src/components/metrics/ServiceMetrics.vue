411fa0a6485fb6f8bb5825773c4fcbc2de64d6a3
nzomkxia <z82507200@gmail.com>
2019-03-21 10:05:58 +0800

    add echarts to implement metrics

--- dubbo-admin-ui/src/components/metrics/ServiceMetrics.vue
+++ dubbo-admin-ui/src/components/metrics/ServiceMetrics.vue
@@ -16,27 +16,227 @@
   -->
 
 <template>
-  <v-container
-    fill-height
-  >
-    <v-layout align-center>
-      <v-flex text-xs-center>
-        <h1 class="display-2 primary--text">{{$t('later.metrics')}}</h1>
-        <v-btn
-          href="#/service"
-          color="primary"
-          outline
-        >
-          {{$t('goIndex')}}
-        </v-btn>
+  <v-container grid-list-xl fluid>
+    <v-layout row wrap>
+      <v-flex lg12>
+        <breadcrumb title="metrics" :items="breads"></breadcrumb>
+      </v-flex>
+      <v-flex xs12 >
+        <search id="serviceSearch" v-model="filter" :submit="submit" :label="$t('searchSingleMetrics')"></search>
+      </v-flex>
+      <v-flex lg4 sm6 xs12>
+        <v-card>
+          <v-card-text>
+            <mini-chart
+              title="Monthly Sales"
+              sub-title="10%"
+              icon="trending_up"
+              :data="dataset.monthVisit"
+              :chart-color="color.blue.base"
+              type="line"
+            >
+            </mini-chart>
+            <mini-chart
+              title="Monthly Sales"
+              sub-title="10%"
+              icon="trending_up"
+              :data="dataset.monthVisit"
+              :chart-color="color.blue.base"
+              type="line"
+            >
+            </mini-chart>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex lg4 sm6 xs12>
+        <v-card>
+          <v-card-text>
+            <mini-chart
+              title="Monthly Sales"
+              sub-title="10%"
+              icon="trending_up"
+              :data="dataset.monthVisit"
+              :chart-color="color.blue.base"
+              type="line"
+            >
+            </mini-chart>
+            <mini-chart
+              title="Monthly Sales"
+              sub-title="10%"
+              icon="trending_up"
+              :data="dataset.monthVisit"
+              :chart-color="color.blue.base"
+              type="line"
+            >
+            </mini-chart>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex lg4 sm6 xs12>
+        <v-card>
+          <v-card-text>
+            <mini-chart
+              title="Monthly Sales"
+              sub-title="10%"
+              icon="trending_up"
+              :data="dataset.monthVisit"
+              :chart-color="color.blue.base"
+              type="line"
+            >
+            </mini-chart>
+            <mini-chart
+              title="Monthly Sales"
+              sub-title="10%"
+              icon="trending_up"
+              :data="dataset.monthVisit"
+              :chart-color="color.blue.base"
+              type="line"
+            >
+            </mini-chart>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex sm12>
+        <h3>{{$t('methodMetrics')}}</h3>
+      </v-flex>
+      <v-flex lg12 >
+        <v-tabs
+          class="elevation-1">
+          <v-tab>
+            {{$t('providers')}}
+          </v-tab>
+          <v-tab>
+            {{$t('consumers')}}
+          </v-tab>
+          <v-tab-item>
+            <v-data-table
+              class="elevation-1"
+              :headers="headers"
+              :items="providerDetails"
+            >
+              <template slot="items" slot-scope="props">
+                <td>{{props.item.service}}</td>
+                <td>{{props.item.method}}</td>
+                <td>{{props.item.qps}}</td>
+                <td>{{props.item.rt}}</td>
+                <td>{{props.item.successRate}}</td>
+              </template>
+            </v-data-table>
+          </v-tab-item>
+          <v-tab-item >
+            <v-data-table
+              class="elevation-1"
+              :headers="headers"
+              :items="consumerDetails"
+            >
+              <template slot="items" slot-scope="props">
+                <td>{{props.item.service}}</td>
+                <td>{{props.item.method}}</td>
+                <td>{{props.item.qps}}</td>
+                <td>{{props.item.rt}}</td>
+                <td>{{props.item.successRate}}</td>
+              </template>
+            </v-data-table>
+          </v-tab-item>
+        </v-tabs>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+  import EChart from '@/util/echart'
+  import Material from 'vuetify/es5/util/colors'
+  import MiniChart from '@/components/public/MiniChart'
+  import Breadcrumb from '@/components/public/Breadcrumb'
+  import Search from '@/components/public/Search'
+  import {
+    monthVisitData,
+    campaignData,
+    locationData,
+    StackData,
+    SinData
+  } from '@/api/chart'
   export default {
-    name: 'ServiceMetrics'
+    name: 'ServiceMetrics',
+    components: {
+      MiniChart,
+      EChart,
+      Breadcrumb,
+      Search
+    },
+    data () {
+      return {
+        selectedTab: 'tab-1',
+        filter: '',
+        headers: [],
+        providerDetails: [
+          {
+            service: 'a.b.c.d',
+            method: 'aaaa~ICS',
+            qps: '0.58',
+            rt: '111',
+            successRate: '100%'
+          },
+          {
+            service: 'a.b.c.f',
+            method: 'bbbb~ICS',
+            qps: '0.87',
+            rt: '120',
+            successRate: '90%'
+          }
+
+        ],
+        consumerDetails: [],
+        option: null,
+        dataset: {
+          sinData: SinData,
+          monthVisit: monthVisitData,
+          campaign: campaignData,
+          location: locationData,
+          stackData: StackData
+        },
+        color: Material,
+        breads: [
+          {
+            text: 'metrics',
+            href: ''
+          }
+        ]
+
+      }
+    },
+    methods: {
+      submit: function () {
+      },
+      setHeaders: function () {
+        this.headers = [
+          {
+            text: this.$t('service'),
+            value: 'service'
+          },
+          {
+            text: this.$t('method'),
+            value: 'method'
+          },
+          {
+            text: this.$t('qps'),
+            value: 'qps'
+          },
+          {
+            text: this.$t('rt'),
+            value: 'rt'
+          },
+          {
+            text: this.$t('successRate'),
+            value: 'successRate'
+          }
+        ]
+      }
+    },
+    mounted: function () {
+      this.setHeaders()
+    }
   }
 </script>
 
