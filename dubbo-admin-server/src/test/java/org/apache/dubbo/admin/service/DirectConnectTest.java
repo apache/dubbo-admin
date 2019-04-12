d3640ccfba1ce215fe6f98e5f44f25cf394a021e
panshengwei <m13201628570@163.com>
2019-04-12 13:44:24 +0800

    deal import problems

--- dubbo-admin-server/src/test/java/org/apache/dubbo/admin/service/DirectConnectTest.java
+++ dubbo-admin-server/src/test/java/org/apache/dubbo/admin/service/DirectConnectTest.java
@@ -1,7 +1,5 @@
 package org.apache.dubbo.admin.service;
 
-import org.apache.dubbo.admin.controller.MetricsCollectController;
-import org.apache.dubbo.admin.service.impl.MetrcisCollectServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
