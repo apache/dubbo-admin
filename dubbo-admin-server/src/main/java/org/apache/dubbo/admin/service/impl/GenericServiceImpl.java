f6f526cacc1d016a3feabff853741820f0bdea6d
charish00 <charish00@163.com>
2019-09-09 00:46:54 +0800

    配置文件缺少address，回滚GenericServiceImpl.java

--- dubbo-admin-server/src/main/java/org/apache/dubbo/admin/service/impl/GenericServiceImpl.java
+++ dubbo-admin-server/src/main/java/org/apache/dubbo/admin/service/impl/GenericServiceImpl.java
@@ -17,48 +17,47 @@
 
 package org.apache.dubbo.admin.service.impl;
 
-import java.util.List;
+import org.apache.dubbo.admin.common.util.Tool;
+import org.apache.dubbo.config.ApplicationConfig;
+import org.apache.dubbo.config.ReferenceConfig;
+import org.apache.dubbo.config.RegistryConfig;
+import org.apache.dubbo.registry.Registry;
+import org.apache.dubbo.rpc.service.GenericService;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
-import org.apache.dubbo.admin.common.util.Tool;
-import org.apache.dubbo.admin.model.domain.Provider;
-import org.apache.dubbo.admin.service.ProviderService;
-import org.apache.dubbo.config.ApplicationConfig;
-import org.apache.dubbo.config.ReferenceConfig;
-import org.apache.dubbo.rpc.service.GenericService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 @Component
 public class GenericServiceImpl {
-    private ProviderService providerService;
-    private ApplicationConfig applicationConfig;
-    
-    @Autowired
-    public GenericServiceImpl(ProviderService providerService) {
-        this.providerService = providerService;
+    private RegistryConfig registryConfig;
+    private final Registry registry;
+
+    public GenericServiceImpl(Registry registry) {
+        this.registry = registry;
     }
 
     @PostConstruct
     public void init() {
-        applicationConfig = new ApplicationConfig();
-        applicationConfig.setName("dubbo-admin");
+        registryConfig = new RegistryConfig();
+        registryConfig.setAddress(registry.getUrl().getProtocol() + "://" + registry.getUrl().getAddress());
+        registryConfig.setGroup(registry.getUrl().getParameter("group"));
+
     }
 
     public Object invoke(String service, String method, String[] parameterTypes, Object[] params) {
-        List<Provider> providers = providerService.findByService(service);
+
+        ApplicationConfig applicationConfig = new ApplicationConfig();
+        applicationConfig.setName("dubbo-admin");
+        applicationConfig.setRegistry(registryConfig);
 
         ReferenceConfig<GenericService> reference = new ReferenceConfig<>();
         String group = Tool.getGroup(service);
         String version = Tool.getVersion(service);
         String interfaze = Tool.getInterface(service);
-        reference.setGeneric(true);
         reference.setApplication(applicationConfig);
         reference.setInterface(interfaze);
         reference.setVersion(version);
         reference.setGroup(group);
-        reference.setUrl(providers.get(0).getUrl()+"?generic=true&timeout=3000");
         GenericService genericService = reference.get();
 
         return genericService.$invoke(method, parameterTypes, params);
