60965c17a4a780ae91135d18a4cf6a6f3c3c1bc4
Huang Yunkun <htynkn@gmail.com>
2020-07-04 17:40:41 +0800

    try upgrade to webpack 4

--- dubbo-admin-ui/vue.config.js
+++ dubbo-admin-ui/vue.config.js
@@ -1,3 +1,24 @@
+const path = require('path');
+
 module.exports = {
-  outputDir: "target/dist"
+  outputDir: "target/dist",
+  lintOnSave: "warning",
+  devServer: {
+    port: 8082,
+    historyApiFallback: {
+      rewrites: [
+        {from: /.*/, to: path.posix.join('/', 'index.html')},
+      ],
+    },
+    publicPath: '/',
+    proxy: {
+      '/': {
+        target: 'http://localhost:8080/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/': '/'
+        }
+      }
+    }
+  }
 };
