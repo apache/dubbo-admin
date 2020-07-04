2b9476ee5d3613f0e115d0800bc02b7a5f9c6dc3
Huang YunKun <htynkn@gmail.com>
2020-07-04 19:54:39 +0800

    reduce vendor.js file size and enable gzip (#630)

--- dubbo-admin-ui/src/main.js
+++ dubbo-admin-ui/src/main.js
@@ -22,9 +22,9 @@ import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
-import { store } from './store'
+import {store} from './store'
 import Notify from './components/public/notify'
-import { AXIOS } from './components/http-common'
+import {AXIOS} from './components/http-common'
 import i18n from './lang'
 import ECharts from 'vue-echarts/components/ECharts'
 import 'echarts/lib/chart/line'
@@ -49,13 +49,11 @@ Vue.component('chart', ECharts)
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
   store,
   i18n,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app')
 
 router.beforeEach((to, from, next) => {
   if (to.matched.some(record => record.meta.requireLogin)) {
---
+++ dubbo-admin-ui/vue.config.js
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const path = require('path');
+
+module.exports = {
+  outputDir: "target/dist",
+  lintOnSave: "warning",
+  devServer: {
+    port: 8082,
+    historyApiFallback: {
+      rewrites: [
+        {from: /.*/, to: path.posix.join('/', 'index.html')},
+      ],
+    },
+    publicPath: '/',
+    proxy: {
+      '/': {
+        target: 'http://localhost:8080/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/': '/'
+        }
+      }
+    }
+  },
+  configureWebpack: {
+    performance: {
+      hints: false
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          reactBase: {
+            name: 'braceBase',
+            test: (module) => {
+              return /brace/.test(module.context);
+            },
+            chunks: 'initial',
+            priority: 10,
+          },
+          common: {
+            name: 'vendor',
+            chunks: 'initial',
+            priority: 2,
+            minChunks: 2,
+          },
+        }
+      }
+    }
+  }
+};
