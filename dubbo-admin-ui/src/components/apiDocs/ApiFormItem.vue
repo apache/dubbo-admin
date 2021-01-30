02afb99ce15a2a97c3ef6e85479e9a2b53014b6c
邪影oO <213539@qq.com>
2021-01-30 19:27:02 +0800

    #686 之前修复逻辑有问题,重新修复 (#688)

--- dubbo-admin-ui/src/components/apiDocs/ApiFormItem.vue
+++ dubbo-admin-ui/src/components/apiDocs/ApiFormItem.vue
@@ -111,6 +111,32 @@
       style="height:300px;"
       outline
     ></json-editor2>
+    <v-text-field
+      v-else-if="formItemInfo.get('htmlType')==='DATE_SELECTOR'"
+      :id="buildItemId()"
+      :name="buildItemId()"
+      :ref="buildItemId()"
+      :label="formItemInfo.get('docName')"
+      :placeholder="formItemInfo.get('example')"
+      :value="buildDefaultValue()"
+      :required="formItemInfo.get('required')"
+      :rules="[requiredCheck]"
+      @change="itemChange($event)"
+      outline
+    ></v-text-field>
+    <v-text-field
+      v-else-if="formItemInfo.get('htmlType')==='DATETIME_SELECTOR'"
+      :id="buildItemId()"
+      :name="buildItemId()"
+      :ref="buildItemId()"
+      :label="formItemInfo.get('docName')"
+      :placeholder="formItemInfo.get('example')"
+      :value="buildDefaultValue()"
+      :required="formItemInfo.get('required')"
+      :rules="[requiredCheck]"
+      @change="itemChange($event)"
+      outline
+    ></v-text-field>
     <span v-else>{{$t('apiDocsRes.apiForm.unsupportedHtmlTypeTip')}}</span>
   </div>
 </template>
@@ -147,11 +173,14 @@ export default {
   },
   methods: {
     buildItemId () {
-      const result = this.formItemInfo.get('paramType') + '@@' +
+      let result = this.formItemInfo.get('paramType') + '@@' +
       this.formItemInfo.get('paramIndex') + '@@' +
       this.formItemInfo.get('javaType') + '@@' +
       this.formItemInfo.get('name') + '@@' +
       this.formItemInfo.get('htmlType')
+      if (this.formItemInfo.get('methodParam')) {
+        result = result + '@@' + this.formItemInfo.get('methodParam')
+      }
       return result
     },
     requiredCheck (value) {
@@ -199,7 +228,7 @@ export default {
       return this.selectDefaultValue
     },
     buildJsonDefaultValue () {
-      var defaultValue = JSON.parse(this.formItemInfo.get('subParamsJson'))
+      var defaultValue = this.formItemInfo.get('subParamsJson') === '' ? this.formItemInfo.get('subParamsJson') : JSON.parse(this.formItemInfo.get('subParamsJson'))
       this.formValues.set(this.buildItemId(), defaultValue)
       return defaultValue
     },
