d7fb77d9c06eae94ad113287aa47b202c931e166
tangziran <823013778@qqcom>
2021-03-02 15:32:14 +0800

    * fix #697 bug.

--- dubbo-admin-ui/src/components/http-common.js
+++ dubbo-admin-ui/src/components/http-common.js
@@ -21,35 +21,35 @@ import router from '@/router'
 import i18n from '@/lang'
 
 let instance = axios.create({
-    baseURL: '/api/dev'
+  baseURL: '/api/dev'
 })
 
 instance.interceptors.request.use(config => {
-    let token = localStorage.getItem('token')
-    if (token) {
-        config.headers.Authorization = token
-    }
-    return config
+  let token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = token
+  }
+  return config
 })
 
 instance.interceptors.response.use((response) => {
-    return response
+  return response
 }, (error) => {
-    if (error.message.indexOf('Network Error') >= 0) {
-        Vue.prototype.$notify.error('Network error, please check your network settings!')
-    } else if (error.response.status === HttpStatus.UNAUTHORIZED) {
-        localStorage.removeItem('token')
-        localStorage.removeItem('username')
-        Vue.prototype.$notify.error(i18n.t('authFailed'))
-        let paths = location.href.split('#')
-        if (paths.length > 1 && paths[1].startsWith('/login')) {
-            // avoid splicing multiple redirects
-            return
-        }
-        router.push({ path: '/login', query: { redirect: paths.length === 1 ? '/' : paths[1] } })
-    } else if (error.response.status >= HttpStatus.BAD_REQUEST) {
-        Vue.prototype.$notify.error(error.response.data.message)
+  if (error.message.indexOf('Network Error') >= 0) {
+    Vue.prototype.$notify.error('Network error, please check your network settings!')
+  } else if (error.response.status === HttpStatus.UNAUTHORIZED) {
+    localStorage.removeItem('token')
+    localStorage.removeItem('username')
+    Vue.prototype.$notify.error(i18n.t('authFailed'))
+    let paths = location.href.split('#')
+    if (paths.length > 1 && paths[1].startsWith('/login')) {
+      // avoid splicing multiple redirects
+      return
     }
+    router.push({path: '/login', query: {redirect: paths.length === 1 ? '/' : paths[1]}})
+  } else if (error.response.status >= HttpStatus.BAD_REQUEST) {
+    Vue.prototype.$notify.error(error.response.data.message)
+  }
 })
 
 export const AXIOS = instance
