85a5e54c1725a1fdf6930c694953d56cf621f0d0
htynkn <htynkn@gmail.com>
2021-08-26 16:57:53 +0800

    update dockerfile and enable autotest

---
+++ docker/0.3.0/test.sh
@@ -0,0 +1,19 @@
+LOOP_SIZE=60
+i=0
+
+while [[ $i -lt LOOP_SIZE ]]; do
+	status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://admin:8080/checkAlive)
+
+  if [[ "$status_code" -eq 200 ]] ; then
+    echo "Tests passed!"
+    exit 0
+  else
+    curl -v http://admin:8080/checkAlive
+    echo "status is incorrect, waiting for next turn"
+  fi
+	sleep 5
+	i=$i+1
+done
+
+echo "Tests failed!"
+exit 1
