1de2488e89b96c9766e3e603f25d9123d2c2b3b3
htynkn <htynkn@gmail.com>
2021-08-26 17:18:13 +0800

    fix test script

--- docker/0.3.0/test.sh
+++ docker/0.3.0/test.sh
@@ -2,13 +2,13 @@ LOOP_SIZE=60
 i=0
 
 while [[ $i -lt LOOP_SIZE ]]; do
-	status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://admin:8080/checkAlive)
+	status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://admin:8080)
 
   if [[ "$status_code" -eq 200 ]] ; then
     echo "Tests passed!"
     exit 0
   else
-    curl -v http://admin:8080/checkAlive
+    curl -v http://admin:8080
     echo "status is incorrect, waiting for next turn"
   fi
 	sleep 5
