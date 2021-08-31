9b3095bb3705db475996846bec4e4096c0dc109f
haoyann <1064645534@qq.com>
2021-08-31 11:48:19 +0800

    fix checkstyle

--- dubbo-admin-server/src/main/java/org/apache/dubbo/admin/service/impl/InstanceRegistryQueryHelper.java
+++ dubbo-admin-server/src/main/java/org/apache/dubbo/admin/service/impl/InstanceRegistryQueryHelper.java
@@ -27,7 +27,6 @@ import org.apache.dubbo.registry.client.ServiceInstance;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
