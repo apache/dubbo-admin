c87351a54ed8d45147bf282d7aa9f75da2fdaf80
璟源 <jingyuan.mj@alibaba-inc.com>
2022-05-26 13:03:32 +0800

    enable swagger by default

--- dubbo-admin-server/src/main/java/org/apache/dubbo/admin/config/SwaggerConfiguration.java
+++ dubbo-admin-server/src/main/java/org/apache/dubbo/admin/config/SwaggerConfiguration.java
@@ -30,7 +30,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-@ConditionalOnProperty(name = "swagger.enable", havingValue = "true")
+@ConditionalOnProperty(name = "swagger.enable", havingValue = "true", matchIfMissing = true)
 public class SwaggerConfiguration {
     @Bean
     public Docket createRestApi() {
