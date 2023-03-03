074e8ba1a16afab1ad36458ce19b06cb839058af
Albumen Kevin <jhq0812@gmail.com>
2023-03-03 10:22:05 +0800

    Fix license

--- dubbo-admin-ui/src/components/test/ServiceTest.vue
+++ dubbo-admin-ui/src/components/test/ServiceTest.vue
@@ -1,19 +1,19 @@
 <!--
-  - Licensed to the Apache Software Foundation (ASF) under one or more
-  - contributor license agreements.  See the NOTICE file distributed with
-  - this work for additional information regarding copyright ownership.
-  - The ASF licenses this file to You under the Apache License, Version 2.0
-  - (the "License"); you may not use this file except in compliance with
-  - the License.  You may obtain a copy of the License at
-  -
-  -     http://www.apache.org/licenses/LICENSE-2.0
-  -
-  - Unless required by applicable law or agreed to in writing, software
-  - distributed under the License is distributed on an "AS IS" BASIS,
-  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  - See the License for the specific language governing permissions and
-  - limitations under the License.
-  -->
+  ~ Licensed to the Apache Software Foundation (ASF) under one or more
+  ~ contributor license agreements.  See the NOTICE file distributed with
+  ~ this work for additional information regarding copyright ownership.
+  ~ The ASF licenses this file to You under the Apache License, Version 2.0
+  ~ (the "License"); you may not use this file except in compliance with
+  ~ the License.  You may obtain a copy of the License at
+  ~
+  ~     http://www.apache.org/licenses/LICENSE-2.0
+  ~
+  ~ Unless required by applicable law or agreed to in writing, software
+  ~ distributed under the License is distributed on an "AS IS" BASIS,
+  ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  ~ See the License for the specific language governing permissions and
+  ~ limitations under the License.
+-->
 <template>
   <v-container grid-list-xl fluid>
     <v-layout row wrap>
@@ -81,11 +81,11 @@
 </template>
 
 <script>
-  import JsonEditor from '@/components/public/JsonEditor'
-  import Search from '@/components/public/Search'
-  import Breadcrumb from '@/components/public/Breadcrumb'
+import JsonEditor from '@/components/public/JsonEditor'
+import Search from '@/components/public/Search'
+import Breadcrumb from '@/components/public/Breadcrumb'
 
-  export default {
+export default {
     name: 'ServiceTest',
     components: {
       JsonEditor,
