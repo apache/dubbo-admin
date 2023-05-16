9bc2382c223a582838719d313fa61b6a39f37c11
chickenlj <ken.lj.hz@gmail.com>
2023-05-16 19:30:38 +0800

    add license header

--- dubbo-admin-ui/jest.config.js
+++ dubbo-admin-ui/jest.config.js
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
   verbose: true, // 多于一个测试文件运行时展示每个测试用例测试通过情况
