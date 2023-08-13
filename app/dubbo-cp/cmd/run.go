14db91636bcd12bad5311f90fe2d7bda2b8241bc
wuyan-dushang <1550693033@qq.com>
2023-08-13 21:32:31 +0800

    fix ci

--- app/dubbo-cp/cmd/run.go
+++ app/dubbo-cp/cmd/run.go
@@ -33,7 +33,6 @@ import (
 	"github.com/apache/dubbo-admin/pkg/core/cmd"
 	"github.com/apache/dubbo-admin/pkg/core/logger"
 	"github.com/apache/dubbo-admin/pkg/cp-server"
-	"github.com/apache/dubbo-admin/pkg/dds"
 	"github.com/spf13/cobra"
 )
 
@@ -89,9 +88,9 @@ func newRunCmdWithOpts(opts cmd.RunCmdOpts) *cobra.Command {
 				logger.Sugar().Error(err, "unable to set up authority")
 			}
 
-			if err := dds.Setup(rt); err != nil {
-				logger.Sugar().Error(err, "unable to set up dds")
-			}
+			//if err := dds.Setup(rt); err != nil {
+			//	logger.Sugar().Error(err, "unable to set up dds")
+			//}
 
 			if err := snp.Setup(rt); err != nil {
 				logger.Sugar().Error(err, "unable to set up snp")
