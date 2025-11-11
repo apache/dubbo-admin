80b73fe5942d0121c1a14d3e3a829e363015cc46
helltab <helltab@apache.org>
2025-11-11 19:36:44 +0800

    doc(build): build ui

--- app/dubbo-ui/dist/admin/assets/serverInfo-j8z5RY-E.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./request-8jI_GZey.js";const a=t=>e({url:"/overview",method:"get",params:t}),o=t=>e({url:"/metadata",method:"get",params:t});export{o as a,a as g};
--- app/dubbo-ui/dist/admin/assets/tab1-y_fNgbfH.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as c}from"./index-hmLAZQYT.js";const o={},_={class:"__container_tabDemo"};function a(n,s){return c(),t("div",_,"tab1")}const f=e(o,[["render",a]]);export{f as default};
