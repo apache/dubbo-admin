6dff50f7bf6cc700f3c058461673ee39d484fa08
robocanic <robocanic@gmail.com>
2026-01-10 14:59:13 +0800

    fix ci

---
+++ app/dubbo-ui/dist/admin/assets/serverInfo-XmLQKnSN.js
@@ -0,0 +1,1 @@
+import{r as e}from"./request-FtkW8_Rp.js";const a=t=>e({url:"/overview",method:"get",params:t}),o=t=>e({url:"/metadata",method:"get",params:t});export{o as a,a as g};
---
+++ app/dubbo-ui/dist/admin/assets/tab1-cytVxWuT.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as c}from"./index-pQAIy9zr.js";const o={},_={class:"__container_tabDemo"};function a(n,s){return c(),t("div",_,"tab1")}const f=e(o,[["render",a]]);export{f as default};
