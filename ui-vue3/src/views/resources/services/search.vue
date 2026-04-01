4eee43b7d97e9507001a532b16fc50c92fe5a56a
MoChengqian <2972013548@qq.com>
2026-04-01 10:46:57 +0800

    refactor(service): remove providerAppName from service pages and use service identity end-to-end

--- ui-vue3/src/views/resources/services/search.vue
+++ ui-vue3/src/views/resources/services/search.vue
@@ -20,10 +20,7 @@
       <template #bodyCell="{ column, record, text }">
         <template v-if="column.dataIndex === 'serviceName'">
           <a-tooltip :title="text">
-            <span
-              class="app-link"
-              @click="viewDistribution(text, record.group, record.version, record.providerAppName)"
-            >
+            <span class="app-link" @click="viewDistribution(text, record.group, record.version)">
               <b>
                 <Icon
                   style="margin-bottom: -2px"
@@ -72,11 +69,6 @@ const columns = [
     key: 'group',
     dataIndex: 'group'
   },
-  // {
-  //   title: 'provider',
-  //   key: 'provider',
-  //   dataIndex: 'providerAppName'
-  // },
   {
     title: 'avgQPS',
     key: 'avgQPS',
@@ -140,7 +132,7 @@ const searchDomain = reactive(
       {
         label: 'serviceName',
         param: 'keywords',
-        placeholder: 'typeAppName',
+        placeholder: 'serviceName',
         defaultValue: query,
         style: {
           width: '200px'
@@ -160,16 +152,10 @@ searchDomain.tableStyle = {
   scrollY: '367px'
 }
 
-const viewDistribution = (
-  serviceName: string,
-  group: string,
-  version: string,
-  providerAppName?: string
-) => {
+const viewDistribution = (serviceName: string, group: string, version: string) => {
   router.push({
     name: 'distribution',
-    params: { pathId: serviceName, group, version },
-    query: providerAppName ? { providerAppName } : {}
+    params: { pathId: serviceName, group, version }
   })
 }
 
